f8f8538a2820b0da61c1aee76a40fb175d162702
Cascerole <121085575+Ananivers-ultraIOI@users.noreply.github.com>
2024-08-13 17:03:25 +0200

    register xml done

--- app/src/main/java/be/heh/kotlinproject_dstheh/RegisterActivity.kt
+++ app/src/main/java/be/heh/kotlinproject_dstheh/RegisterActivity.kt
@@ -1,6 +1,9 @@
 package be.heh.kotlinproject_dstheh
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,5 +17,15 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+    fun onRegisterClickManager(v: View){
+        when(v.id){
+            binding.btRegister.id -> Toast.makeText(applicationContext,"register", Toast.LENGTH_LONG).show()
+            binding.tvRegisterConnexion.id -> toConnexion()
+        }
+    }
+    fun toConnexion(){
+        val iConnection= Intent(this,MainActivity::class.java)
+        startActivity(iConnection)
     }
 }
